2761b7ba94e5d92d00dae343c0d4f96cea74a472
cunyizju <cunyicom@outlook.com>
2025-06-19 12:37:33 +0200

    phase change 2022 okay

---
+++ PhaseChange2022/fetch.sh
@@ -0,0 +1,12 @@
+abq2022 fetch job=oneelem_phase_trans_s5140
+abq2022 fetch job=oneelem_phase_trans_ti64
+abq2022 fetch job=oneelem_phase_trans_ti64_amprint
+
+# abq2022 job=oneelem_phase_trans_s5140
+# abq2022 job=oneelem_phase_trans_ti64
+# abq2022 job=oneelem_phase_trans_ti64_amprint
+
+abq2022 fetch job=ABQ_phase_trans_types
+abq2022 fetch job=amp_temp_amprint
+abq2022 fetch job=mat_phase_trans_ti64
+abq2022 fetch job=mat_phase_trans_s5140
